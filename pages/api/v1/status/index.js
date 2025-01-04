31552d77362795dfab582d8194caf113ece6ce99
Lucas Paggiaro <75700702+lucasppimentel@users.noreply.github.com>
2025-01-04 21:30:31 +0000

    Teste do endpoint `/status` com `Jest`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,14 +4,14 @@ async function status(request, response) {
   const version = await database.query("SHOW server_version;");
   const maxCon = await database.query("SHOW max_connections;");
   const activeCon = await database.query(
-    `SELECT COUNT(*) FROM pg_stat_activity WHERE datname = '${process.env.POSTGRES_DB}';`,
+    `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${process.env.POSTGRES_DB}';`,
   );
 
   response.status(200).json({
     timestamp: new Date().toISOString(),
     database: {
       version: version.rows[0].server_version,
-      max_connections: maxCon.rows[0].max_connections,
+      max_connections: parseInt(maxCon.rows[0].max_connections, 10),
       active_connections: activeCon.rows[0].count,
     },
   });
