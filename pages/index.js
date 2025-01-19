31ac4ffff95461edbf19e10f5e66f915b0b05bf7
Lucas Paggiaro <75700702+lucasppimentel@users.noreply.github.com>
2025-01-19 14:11:48 +0000

    Criando a página `Em Construção`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import styles from "assets/styles/Home.module.css";
+import imageSrc from "assets/images/home.webp";
+
 function Home() {
-  return <h1> Teste </h1>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <Image
+          src={imageSrc}
+          alt="Under Construction"
+          width={500}
+          height={300}
+        />
+        <h1 className={styles.title}>Em construção</h1>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
